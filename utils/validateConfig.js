a291f339b2d46774afa502a778d512459a7ec7b7
HaolinHom <haolinhom@gmail.com>
2020-08-25 10:12:38 +0800

    fix: validateConfig;

--- utils/validateConfig.js
+++ utils/validateConfig.js
@@ -58,7 +58,7 @@ function validateConfig(cfg) {
     } else if (moduleType !== 'string') {
       std.error(ERROR.CONFIG_MODULE_TYPE_NOT_STRING);
       result = false;
-    } else if (![CONFIG.ES_MODULE, CONFIG.NODE_MODULE].includes(moduleType)) {
+    } else if (![CONFIG.ES_MODULE, CONFIG.NODE_MODULE].includes(cfg.moduleType)) {
       std.error(`${ERROR.CONFIG_MODULE_TYPE_ILLEGAL}(must be 'es' or 'node')`);
       result = false;
     }
